d6cf8b9fa9575e8e1ffeb5ce4c5c7b0182c113dc
Wagner Braga <wfbraga@hotmail.com>
2025-02-20 15:09:52 -0300

    Estilizando o input the prompts

--- src/App.js
+++ src/App.js
@@ -7,11 +7,52 @@ import { useState } from 'react'
 
 import { SideMenu } from './components/SideMenu/SideMenu'
 
+import { ChatMessage } from './components/ChatMessage/ChatMessage';
+
 function App() {
+  const [input, setInput] = useState('')
+  const [chatlog, setChatlog] = useState([{
+      user: "gpt",
+      message: "Olá, eu sou o GPT-3, como posso te ajudar?"
+    }])
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    let response = await makeRequest({prompt: input})
+    response = response.data.split('\n').map(line => `<p>${line}<p/>`)
+    setChatlog([...chatlog, {
+      user: 'me',
+      message: `${input}`
+    },
+    {
+      user: 'gpt',
+      message: `${response}`
+    }
+    ])
+    setInput('')
+  } 
+
   return (
     <div className="App">
       <SideMenu></SideMenu>
-      <h1>Is working!!!</h1>
+      <section className='chatbox'>
+        <div className='chatlog'>
+          { chatlog.map((message, index) => (
+              <ChatMessage key={index} message={message} />
+          ))}
+
+        </div>
+        <div className='chat-input-holder'>
+          <form onSubmit={handleSubmit}>
+            <input
+              rows='1'
+              className='chat-input-textarea'
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
